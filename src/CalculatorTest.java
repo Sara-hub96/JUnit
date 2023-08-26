15cb3a10e053cb743e31fc8619d9427639efe027
Sara <saracoppola996@gmail.com>
2023-08-26 16:01:42 +0200

    first commit

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -7,24 +7,24 @@ class CalculatorTest {
     @Test
     void tenMinusFiveShouldEqualFive(){
         Calculator calculator = new Calculator();
-        assertEquals(5, Calculator.subtraction(10,5));
+        assertEquals(5, calculator.subtraction(10,5));
     }
 
     @Test
     void twelveMinusThreeShouldEqualNine(){
         Calculator calculator = new Calculator();
-        assertEquals(9,Calculator.subtraction(12,3));
+        assertEquals(9, calculator.subtraction(12,3));
     }
 
     @Test
     void fourTimesEightShouldEqualThirtyTwo() {
         Calculator calculator = new Calculator();
-        assertEquals(32,Calculator.multiplication(4,8));
+        assertEquals(32, calculator.multiplication(4,8));
     }
 
     @Test
     void twoTimesZeroShouldEqualZero() {
         Calculator calculator = new Calculator();
-        assertEquals(0,Calculator.multiplication(2,0));
+        assertEquals(0, calculator.multiplication(2,0));
     }
 }
